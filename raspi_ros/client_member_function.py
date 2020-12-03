f26f16a1468e383325c64a15ddc1851bfdcb7949
r.pahic <r.pahic@gmail.com>
2020-12-03 13:49:32 +0000

    created client and server for seting config

--- raspi_ros/client_member_function.py
+++ raspi_ros/client_member_function.py
@@ -1,11 +1,9 @@
-import sys
-
-from example_interfaces.srv import AddTwoInts
-import rclpy
-from rclpy.node import Node
+import rospy
+import rosservice
+from digital_interface_msgs.srv import ConfigRead, ConfigSet,ConfigSetRequest
 
 
-class MinimalClientAsync(Node):
+class MinimalClientAsync(object):
 
     def __init__(self):
         super().__init__('minimal_client_async')
@@ -20,28 +18,102 @@ class MinimalClientAsync(Node):
         self.future = self.cli.call_async(self.req)
 
 
+
 def main(args=None):
-    rclpy.init(args=args)
 
-    minimal_client = MinimalClientAsync()
-    minimal_client.send_request()
 
-    while rclpy.ok():
-        rclpy.spin_once(minimal_client)
-        if minimal_client.future.done():
-            try:
-                response = minimal_client.future.result()
-            except Exception as e:
-                minimal_client.get_logger().info(
-                    'Service call failed %r' % (e,))
+    #rospy.create_client()
+    #rospy.create_client()
+  
+
+    #get all services
+    service_list=rosservice.get_service_list()
+
+    #search for the configuration services
+    raspi_services=[]
+    for i in service_list:
+        if 'config_set_new' in i:
+            raspi_services.append(i)
+
+    print('Hello! I\'m Rassberry ROS configuration client. I have next Raspberries in my reach:')
+
+    j=0
+    for i in raspi_services:
+        j=j+1
+        print(str(j)+'.  '+str(i))
+
+
+    chosen_raspi=int(input('Choose number of the one that you want to configure:'))
+    print('You have choosen: ' + str(chosen_raspi))
+
+
+    chosen_template=int((raw_input('Configure from now active configuration (write 1) or empty template (write 2)? (default = 1)')) or 1)
+    if chosen_template==1:
+        print('Active configuration it is.')
+        read_proxy= rospy.ServiceProxy('config_read_current', ConfigRead)
+
+    elif chosen_template==2:
+        print('Empty template it is.')
+        read_proxy= rospy.ServiceProxy('config_read_template', ConfigRead)
+
+    template_msg = read_proxy().config
+
+    print(template_msg)
+
+    pin_number=int(input('Which pin you want to configure? (write number pin or write 0 if you are finish):'))
+
+    while (pin_number!=0):
+        print('Current pin configuration:')
+        print(template_msg.pin_configs[pin_number-1])
+        
+        wrong_config=True
+        while wrong_config:
+            chosen_pin_config=str(raw_input('Write desired configuration:'))
+       
+
+            if chosen_pin_config in template_msg.pin_configs[pin_number-1].available_config:
+
+                template_msg.pin_configs[pin_number-1].actual_config=chosen_pin_config
+                wrong_config=False
+
             else:
-                minimal_client.get_logger().info(
-                    'Result of add_two_ints: for %d + %d = %d' %
-                    (minimal_client.req.a, minimal_client.req.b, response.sum))
-            break
+                print('wrong config, try again with this!')
+                print(template_msg.pin_configs[pin_number-1].available_config)
 
-    minimal_client.destroy_node()
-    rclpy.shutdown()
+        template_msg.pin_configs[pin_number-1].service_name=str(raw_input('Write desired service name:'))
+
+        pin_number=int(input('Which pin you want to configure next? (write number pin or write 0 if you are finish):'))
+
+
+    
+    print('Sending config')
+    write_proxy= rospy.ServiceProxy('config_set_new', ConfigSet)
+
+    response = write_proxy(template_msg)
+      
+
+
+    if False:
+        rclpy.init(args=args)
+
+        minimal_client = MinimalClientAsync()
+        minimal_client.send_request()
+
+        while rclpy.ok():
+            rclpy.spin_once(minimal_client)
+            if minimal_client.future.done():
+                try:
+                    response = minimal_client.future.result()
+                except Exception as e:
+                    minimal_client.get_logger().info(
+                        'Service call failed %r' % (e,))
+                else:
+                    minimal_client.get_logger().info(
+                        'Result of add_two_ints: for %d + %d = %d' %
+                        (minimal_client.req.a, minimal_client.req.b, response.sum))
+                break
+
+
 
 
 if __name__ == '__main__':
--- raspi_ros/service_tool_manager.py
+++ raspi_ros/service_tool_manager.py
@@ -1,85 +1,87 @@
 
-from digital_interface_msgs.msg import DigitalState
+from digital_interface_msgs.msg import RaspiConfig
+from digital_interface_msgs.srv import ConfigRead, ConfigSet,ConfigReadResponse,ConfigSetResponse
+from std_srvs.srv import Trigger
 
-from digital_interface_msgs.srv import ConfigRead, ConfigSet,ConfigReadResponse
-
-from gpiozero import DigitalOutputDevice
-import yaml 
 import rospy
 
+import yaml 
+from rospy_message_converter import message_converter
 
-
-def config_to_dict(config):
-    config_dict={}
-
-
-    for i in config.__slots__:
-       config_dict.update({i:getattr(config,i)})
-    return config_dict
-
+import os.path
 
 
 class ManagerService(object):
 
     def __init__(self):
 
-
-
-        self.sendback_current_config(ConfigRead)
+        self.path='/ros_ws/src/raspi_ros/config/active_config.yaml'
+        self.template_path='/ros_ws/src/raspi_ros/config/raspberry4_config_template.yaml'
+        rospy.wait_for_service('restart_node')
+        self.restart_proxy= rospy.ServiceProxy('restart_node',Trigger)
+        # service for reading active configuration
         self.read_config_srv = rospy.Service('config_read_current',ConfigRead,  self.sendback_current_config)
+
+       
         self.template_config_srv = rospy.Service('config_read_template',ConfigRead, self.sendback_template_config)
         self.set_config_srv =rospy.Service('config_set_new',ConfigSet, self.change_current_config)
 
 
     def sendback_current_config(self,request):
+
+       # load current config file from yaml
+        with open(self.path, 'r') as file:
+            active_config = yaml.load(file)
+
+        # transform it in ros_messege
+        message = message_converter.convert_dictionary_to_ros_message('digital_interface_msgs/RaspiConfig', active_config) 
+
+        # create and fill response  
         response=ConfigReadResponse()
-        response.config.config_name='test'
-        print('sendback')
-
-        config_dict=config_to_dict(response.config)
-        print(response.config.__slots__)
-        print(config_dict)
-
-
-        dict_file = [{'sports' : ['soccer', 'football', 'basketball', 'cricket', 'hockey', 'table tennis']},
-        {'countries' : ['Pakistan', 'USA', 'India', 'China', 'Germany', 'France', 'Spain']}]
-
-        with open(r'/ros_ws/src/raspi_ros/config/tool_config_file.yaml', 'w') as file:
-            documents = yaml.dump(config_dict, file)
-
-
+        response.config=message
+ 
         return response
+
 
     def sendback_template_config(self,request):
+       # load template config file from yaml
+        with open(self.template_path, 'r') as file:
+            template_config = yaml.load(file)
+
+        # transform it in ros_messege
+        message = message_converter.convert_dictionary_to_ros_message('digital_interface_msgs/RaspiConfig', template_config) 
+
+        # create and fill response  
         response=ConfigReadResponse()
-        response.config.config_name='test'
-        print('sendback')
-
-        config_dict=config_to_dict(response.config)
-
-
-        with open(r'/ros_ws/src/raspi_ros/config/tool_config_file.yaml', 'w') as file:
-            documents = yaml.dump(config_dict, file)
-
-
+        response.config=message
+ 
         return response
 
 
-    def change_current_config(self,request,response):
-        config=request.config
-        response.set=1
 
-        return response
+    def change_current_config(self,request):
+
+     
+        config_dict=message_converter.convert_ros_message_to_dictionary(request.config)
+
+        with open(self.path, 'w') as file:
+            documents = yaml.safe_dump(config_dict, file)
+
+        response_from_reset=self.restart_proxy()
+        response=ConfigSetResponse
+
+        return response(True,'')
 
 
 def main(args=None):
     
-
+    node_name='tool_manager'
+    rospy.init_node(node_name)
     manager_service = ManagerService()
 
-    rospy.spin(manager_service)
+    rospy.spin()
 
-    rospy.shutdown()
+    
 
 
 if __name__ == '__main__':
